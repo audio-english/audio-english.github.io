6f9d7f2681a99f6bb2c0592d5bbd592b7c9b4352
bolein <bolein95@gmail.com>
2020-04-18 14:59:52 +0800

    update bundle.js

--- bundle.js
+++ bundle.js
@@ -8,9 +8,22 @@ $(document).ready(function(e) {
   $("#comshow").change(function() {
     toggleText(".trainer-table .comment", !this.checked)
   })
+  $(".trainer-table td").click(function(e) {
+    if (event.ctrlKey || event.metaKey) return;
+    $(this).toggleClass('hide-text')
+  })
+  $(".trainer-table .play-btn").click(function(e) {
+    const audio_id = $(this).attr('audio');
+    playAudio(audio_id)
+    e.stopPropagation()
+  })
 })
 
 function toggleText(cls, state) {
   $(cls).toggleClass('hide-text', state)
 }
 
+function playAudio(audio) {
+  new Audio(audio).play();
+  return false;
+}
